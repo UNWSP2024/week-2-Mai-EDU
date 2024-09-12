2cb9ccdff13a8425d51164bb12950de8ccddc35a
Mai-EDU <malillie@students.unwsp.edu>
2024-09-12 13:42:45 -0500

    Update program_3.py

--- program_3.py
+++ program_3.py
@@ -1,8 +1,17 @@
+#Programmer: Michael (Mai) Lillie
+#Date: 9/12/24
 def calculate_total_purchase():
-    # A customer in a store is purchasing five items.  
-    # Write a program that asks for each item, 
-    # then displays the subtotal of the sale, 
-    # the amount of sales tax, and the total.  
-    # Assume the sales tax is 7 percent.
+    # Get User Input
+    item1 = float(input('Enter first item\'s cost: '))
+    item2 = float(input('Enter second item\'s cost: '))
+    item3 = float(input('Enter third item\'s cost: '))
+    item4 = float(input('Enter fourth item\'s cost: '))
+    item5 = float(input('Enter fifth item\'s cost: '))
+    # Sum of items, tax, and true total math
+    item_sum = item1 + item2 + item3 + item4 + item5
+    tax = item_sum * 0.07
+    true_sum = tax + item_sum
+    #Print the subtotal, tax, and true total
+    print(f'Subtotal: {item_sum:.2f} \nTax Amount: {tax:.2f} \nTotal: {true_sum:.2f}')
 
 calculate_total_purchase()
